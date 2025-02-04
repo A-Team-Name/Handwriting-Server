8f554f534254ac03e078ac25e1b78a7b398c6de6
jfitz02 <jfitz02@outlook.com>
2025-02-04 17:11:24 +0000

    changed entrypoint port

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 if [ "$FLASK_ENV" = "development" ]; then
     poetry run python ./app.py
 else
-    poetry run gunicorn --bind 0.0.0.0:$PORT app:app
+    poetry run gunicorn --bind 0.0.0.0:$HANDWRITING_PORT app:app
 fi
