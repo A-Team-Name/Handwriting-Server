9237fdef1ab78eca23818f392d38bf883e264c3d
jfitz02 <jfitz02@outlook.com>
2025-02-04 16:55:18 +0000

    changed env variable for port

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def test_gpu():
     return jsonify({'msg': cuda.is_available()})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("HANDWRITING_PORT", 5000))
