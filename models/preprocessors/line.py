897cd83ec0b4e7dc5ed1f4a0885df6a65203cb37
jfitz02 <jfitz02@outlook.com>
2025-02-25 11:04:32 +0000

    line separation

--- models/preprocessors/line.py
+++ models/preprocessors/line.py
@@ -1,6 +1,11 @@
 import numpy as np
 import numpy.typing as npt
-from .preprocessor import Preprocessor
+if __name__ == "__main__":
+    from preprocessor import Preprocessor
+    import doctest
+    doctest.testmod()
+else:
+    from .preprocessor import Preprocessor
 
 class LinePreprocessor(Preprocessor):
     """
@@ -11,16 +16,55 @@ class LinePreprocessor(Preprocessor):
     """
     def __init__(self):
         pass
+    
 
     def preprocess(self, image: npt.NDArray[np.ubyte]) -> list[npt.NDArray[np.ubyte]]:
         """
-        TODO: Line Separation
-        Currently does nothing.
+        Line separation using naive connected component analysis on columns.
 
         Args:
             image (npt.NDArray[np.ubyte]): The image to preprocess.
 
         Returns:
             list[npt.NDArray[np.ubyte]]: The preprocessed images.
+            
+            
+        >>> processor = LinePreprocessor()
+        >>> test_img = np.array([[1,1,1,1,1],[1,1,1,1,1,],[1,0,0,0,1],[1,0,1,1,1],[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1]])
+        >>> processor.preprocess(test_img)
+        [array([[1, 1, 1, 1, 1],
+               [1, 0, 0, 0, 1],
+               [1, 0, 1, 1, 1],
+               [1, 1, 1, 1, 1]]), array([[1, 1, 1, 1, 1],
+               [1, 0, 1, 0, 1],
+               [1, 1, 1, 1, 1]])]
         """
-        return [image]
+        # collapse each row to be min of row
+        
+        min_col = np.min(image, axis=1)
+        
+        # find connected components of value 0 and generate tuple of start and end
+        # of each connected component
+        
+        start = 0
+        end = 0
+        components = []
+        for i in range(len(min_col)):
+            if min_col[i] == 0:
+                end = i
+            else:
+                if start != end:
+                    components.append((start+1, end))
+                start = i
+                end = i
+                
+        # add 1 pixel padding
+        to_return = []
+        for component in components:
+            start, end = component
+            to_return.append(image[start-1:end+2])
+        
+        return to_return
+
+if __name__ == "__main__":
+    doctest.testmod()
