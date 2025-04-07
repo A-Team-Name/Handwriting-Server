5d48ffd6c02b7ef7818f5f48bc02c74937ba44cb
Joshua Fitzmaurice <jfitz02@outlook.com>
2025-04-07 20:01:10 +0100

    added most tests

---
+++ tests/models_test.py
@@ -0,0 +1,46 @@
+import pytest
+from models.models import TransformerModel, ShapeContextsModel, LambdaCNNChar
+from models.output import Output
+import numpy as np
+
+def test_transformer_output():
+    """Test the TransformerModel output"""
+    model = TransformerModel("MrFitzmaurice/TrOCR-Lambda-Calculus", "MrFitzmaurice/TrOCR-Lambda-Calculus")
+    image = np.ones((128, 128), dtype=np.ubyte)*255
+    result = model.predict(image)
+    assert isinstance(result, Output)  # Assuming the output is a string
+    assert len(result.top_preds) > 0  # Check if predictions are made
+    assert len(result.top_probs) > 0  # Check if probabilities are made
+    
+    # For each set of probs in top_probs, ensure it is in descending order
+    for probs in result.top_probs:
+        assert all(probs[i] >= probs[i + 1] for i in range(len(probs) - 1)), "Probabilities are not in descending order"
+        
+    
+def test_cnn_output():
+    """Test the ShapeContextsModel output"""
+    model = LambdaCNNChar()
+    image = np.ones((128, 128), dtype=np.ubyte)*255
+    result = model.predict(image)
+    assert isinstance(result, Output)  # Assuming the output is a string
+    assert len(result.top_preds) > 0  # Check if predictions are made
+    assert len(result.top_probs) > 0  # Check if probabilities are made
+    
+    # For each set of probs in top_probs, ensure it is in descending order
+    for probs in result.top_probs:
+        assert all(probs[i] >= probs[i + 1] for i in range(len(probs) - 1)), "Probabilities are not in descending order"
+        
+    
+def test_shape_output():
+    """Test the ShapeContextsModel output"""
+    model = ShapeContextsModel()
+    image = np.ones((128, 128), dtype=np.ubyte)*255
+    result = model.predict(image)
+    assert isinstance(result, Output)  # Assuming the output is a string
+    assert len(result.top_preds) > 0  # Check if predictions are made
+    assert len(result.top_probs) > 0  # Check if probabilities are made
+    
+    # For each set of probs in top_probs, ensure it is in descending order
+    for probs in result.top_probs:
+        assert all(probs[i] >= probs[i + 1] for i in range(len(probs) - 1)), "Probabilities are not in descending order"
+        
