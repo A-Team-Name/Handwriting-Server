c9692527a89bd4a6a6711d7d88ac5b4054dcf8dc
Asher Harvey-Smith <asherharveysmith@gmail.com>
2025-04-10 11:31:44 +0100

    resolving comments

--- models/models/__init__.py
+++ models/models/__init__.py
@@ -2,4 +2,3 @@ from .transformer import TransformerModel
 from .model import Model
 from .shape_contexts import ShapeContextsModel
 from .lambda_cnn_char import LambdaCNNChar
-from .dummy import DummyModel
